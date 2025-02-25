d1ed10c06b52965c1b05db460cef8528894fcbcf
MastroAndre <mastroandre05@gmail.com>
2025-02-25 10:10:44 +0100

    update es 3c1 e 3c4

--- Lezione_03/3C_1.py
+++ Lezione_03/3C_1.py
@@ -1,12 +1,12 @@
 voto:int = int(input("Inserire un voto: "))
 match voto:
-    case voto if voto >= 1 and voto <= 3:
+    case 1 | 2 | 3:
         print("Gravemente insufficiente")
-    case voto if voto == 4 or voto == 5:
+    case 4 | 5:
         print("Insufficiente")
-    case voto if voto == 6 or voto == 7:
+    case 6 | 7:
         print("Sufficiente")
-    case voto if voto == 8 or voto == 9:
+    case 8 | 9:
         print("Molto buono")
     case 10:
         print("Eccellente")
--- Lezione_03/3C_4.py
+++ Lezione_03/3C_4.py
@@ -1,13 +1,13 @@
 animal:str = str(input("Inserire nome animale: "))
 
 match animal:
-    case _ if animal in ["cane", "gatto", "cavallo", "elefante", "leone"]:
+    case "cane" | "gatto" | "cavallo" | "elefante" | "leone":
         print(f"{animal} appartiene alla categoria dei Mammiferi")
-    case _ if animal in ["serpente", "lucertola", "tartaruga", "coccodrillo"]:
+    case "serpente" | "lucertola" | "tartaruga" | "coccodrillo":
         print(f"{animal} appartiene alla categoria dei Rettili")
-    case _ if animal in ["aquila", "pappagallo", "gufo", "falco"]:
+    case "aquila" | "pappagallo" | "gufo" | "falco":
         print(f"{animal} appartiene alla categoria degli Uccelli ")
-    case _ if animal in ["squalo", "trota", "salmone", "carpa"]:
+    case "squalo" | "trota" | "salmone" | "carpa":
         print(f"{animal} appartiene alla categoria dei Pesci")
     case _:
         print(f"Non so dire la categoria dell'animale '{animal}'.")
